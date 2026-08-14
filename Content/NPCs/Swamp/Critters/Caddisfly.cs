b29d2e38ef6cf33213c8289b1c20afecf9a91f97
Sulfur <vi.inparadise@gmail.com>
2026-08-14 19:54:00 -0400

    Two critters and failed miserably to implement the scorpion

---
+++ Content/NPCs/Swamp/Critters/Caddisfly.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ABMod.Content.NPCs.Swamp.Critters
+{
+    public class Caddisfly : ModNPC
+    {
+        private const int ClonedAIStyle = NPCAIStyleID.Dragonfly;
+        private const int ClonedNPCID = NPCID.RedDragonfly;
+
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[Type] = Main.npcFrameCount[ClonedNPCID];
+
+            NPCID.Sets.CountsAsCritter[Type] = true;
+            NPCID.Sets.TakesDamageFromHostilesWithoutBeingFriendly[Type] = true;
+            NPCID.Sets.TownCritter[Type] = true;
+        }
+
+        public override void SetDefaults()
+		{
+            NPC.CloneDefaults(ClonedNPCID);
+            NPC.width = 30;
+            NPC.height = 20;
+
+            NPC.aiStyle = ClonedAIStyle;
+            AIType = ClonedNPCID;
+            AnimationType = ClonedNPCID;
+		}
+
+        public override void AI()
+        {
+            NPC.rotation = NPC.velocity.X * 0.05f;
+        }
+    }
+}
